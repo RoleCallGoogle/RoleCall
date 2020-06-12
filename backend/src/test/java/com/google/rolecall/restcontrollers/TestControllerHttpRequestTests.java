2fb0e24d60b3db9fe39fc70a8a65c409d54e4dc3
Jared Hirsch <jaredhirsch@google.com>
2020-06-12 23:14:23 +0000

    Typo

--- backend/src/test/java/com/google/rolecall/restcontrollers/TestControllerHttpRequestTests.java
+++ backend/src/test/java/com/google/rolecall/restcontrollers/TestControllerHttpRequestTests.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 
 /**
- * Tests for the /api/test enpoint via reuests to the Spring Application server.
+ * Tests for the /api/test enpoint via http requests to the Spring Application server.
  */
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class TestControllerHttpRequestTests {
