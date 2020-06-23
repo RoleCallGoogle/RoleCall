cd5800dadd02983c01fdf56cabe5ecbde5047445
Jared Hirsch <jaredhirsch@google.com>
2020-06-23 19:53:46 +0000

    Update and initialize executor

--- backend/src/main/java/com/google/rolecall/config/AsyncConfig.java
+++ backend/src/main/java/com/google/rolecall/config/AsyncConfig.java
@@ -9,14 +9,20 @@ import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+/** Asyncronous functionality and  */
 @EnableAsync
 @Configuration
 public class AsyncConfig implements AsyncConfigurer {
 
   @Override
   public Executor getAsyncExecutor() {
-    // TODO: Make customized thread executor  
-    return new ThreadPoolTaskExecutor();
+    // TODO: Make customized thread executor 
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setWaitForTasksToCompleteOnShutdown(true);
+    executor.setAwaitTerminationSeconds(60);
+    executor.setThreadNamePrefix("default_executor_thread");
+    executor.initialize();
+    return executor;
   }
 
   @Override
