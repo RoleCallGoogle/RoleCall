6f74de7179a9412c3cdde1bd0ba6be0cadb20f96
Jared Hirsch <jaredhirsch@google.com>
2020-06-23 20:12:03 +0000

    Update function to run asyncronously

--- backend/src/main/java/com/google/rolecall/restcontrollers/TestController.java
+++ backend/src/main/java/com/google/rolecall/restcontrollers/TestController.java
@@ -1,16 +1,19 @@
 package com.google.rolecall.restcontrollers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.concurrent.CompletableFuture;
+
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /** Initial REST Controller for the RoleCall Proof of Concept (POC). */
 @RestController
-@RequestMapping("/api/")
-public class TestController {
+public class TestController extends AsyncRestEndpoint {
   
-	@RequestMapping("/test")
-	public String sayHello(@RequestParam(value="value", required=false, defaultValue="default") String value) {
-	  return "Hello " + value;
+  @Async
+	@GetMapping("/api/test")
+	public CompletableFuture<String> sayHello(@RequestParam(value="value", required=false, defaultValue="default") String value) {
+	  return CompletableFuture.completedFuture("Hello " + value);
 	}
 }
