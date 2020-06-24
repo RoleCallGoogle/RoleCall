52ae051e708485e293bdf324907c2fbd4d5469d6
Jared Hirsch <jaredhirsch@google.com>
2020-06-24 22:07:38 +0000

    Test asyncronous exception handler for 100% completeness

--- backend/src/main/java/com/google/rolecall/restcontrollers/TestController.java
+++ backend/src/main/java/com/google/rolecall/restcontrollers/TestController.java
@@ -3,6 +3,7 @@ package com.google.rolecall.restcontrollers;
 import java.util.concurrent.CompletableFuture;
 
 import com.google.rolecall.restcontrollers.endpointannotations.AsyncGetEndpoint;
+import com.google.rolecall.restcontrollers.endpointannotations.AsyncPostEndpoint;
 import com.google.rolecall.restcontrollers.endpointannotations.Endpoint;
 
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,5 +15,10 @@ public class TestController extends AsyncRestEndpoint {
 	@AsyncGetEndpoint
 	public CompletableFuture<String> sayHello(@RequestParam(value="value", required=false, defaultValue="default") String value) {
 	  return CompletableFuture.completedFuture("Hello " + value);
+  }
+  
+  @AsyncPostEndpoint
+	public CompletableFuture<String> throwException() {
+	  return CompletableFuture.failedFuture(new Exception("Purposeful exception"));
 	}
 }
