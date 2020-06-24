8be73189f05390cb74f81fc76c33c04c255628fc
Jared Hirsch <jaredhirsch@google.com>
2020-06-24 01:54:40 +0000

    Update endpoint to work asyncronously with new annnotations

--- backend/src/main/java/com/google/rolecall/restcontrollers/AsyncRestEndpoint.java
+++ backend/src/main/java/com/google/rolecall/restcontrollers/AsyncRestEndpoint.java
@@ -1,6 +1,6 @@
 package com.google.rolecall.restcontrollers;
 
-/** Utility methods for REST api enpoint calls */
+
+/** Utility methods for REST api enpoint calls. */
 public abstract class AsyncRestEndpoint {
-  
 }
--- backend/src/main/java/com/google/rolecall/restcontrollers/TestController.java
+++ backend/src/main/java/com/google/rolecall/restcontrollers/TestController.java
@@ -2,17 +2,16 @@ package com.google.rolecall.restcontrollers;
 
 import java.util.concurrent.CompletableFuture;
 
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.web.bind.annotation.GetMapping;
+import com.google.rolecall.restcontrollers.endpointannotations.AsyncGetEndpoint;
+import com.google.rolecall.restcontrollers.endpointannotations.Endpoint;
+
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 /** Initial REST Controller for the RoleCall Proof of Concept (POC). */
-@RestController
+@Endpoint("/api/test")
 public class TestController extends AsyncRestEndpoint {
   
-  @Async
-	@GetMapping("/api/test")
+	@AsyncGetEndpoint
 	public CompletableFuture<String> sayHello(@RequestParam(value="value", required=false, defaultValue="default") String value) {
 	  return CompletableFuture.completedFuture("Hello " + value);
 	}
