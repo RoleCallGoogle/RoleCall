b9aa6e6d46f04970e94c1d716fec9c7159d8bf86
Jared Hirsch <jaredhirsch@google.com>
2020-06-29 22:28:06 +0000

    Add incrementing endpoint

---
+++ backend/src/main/java/com/google/rolecall/restcontrollers/RequestExceptions.java
@@ -0,0 +1,31 @@
+package com.google.rolecall.restcontrollers;
+
+public final class RequestExceptions {
+  
+  public static class InvalidArgumentException extends Exception{
+
+    public InvalidArgumentException(String message) {
+      super(message);
+    }
+
+    private static final long serialVersionUID = 398486992691471219L;
+  } 
+
+  public static class UnsupportedOperationException extends Exception{
+
+    public UnsupportedOperationException(String message) {
+      super(message);
+    }
+
+    private static final long serialVersionUID = 1892786199356723273L;
+  }
+
+  public static class InvalidPermissionsException extends Exception{
+
+    public InvalidPermissionsException(String message) {
+      super(message);
+    }
+
+    private static final long serialVersionUID = 9098340870678943547L;
+  }
+}
