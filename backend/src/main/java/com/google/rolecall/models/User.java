dea33cfd7e279ed841360d048e710a3eb4c72d96
Jared Hirsch <jaredhirsch@google.com>
2020-06-29 20:14:54 +0000

    Remove config (application.properties) from git

--- backend/src/main/java/com/google/rolecall/models/User.java
+++ backend/src/main/java/com/google/rolecall/models/User.java
@@ -6,7 +6,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-/** Basic user information and  */
+/** Basic user information. */
 @Entity
 @Table
 public class User {
