dc2ede3d9034f3f14568f862d2de8d2fd0a21582
Jared Hirsch <jaredhirsch@google.com>
2020-07-07 00:03:53 +0000

    Add another test

--- backend/src/test/java/com/google/rolecall/restcontrollers/IncrementUserLoginHttpRequestTests.java
+++ backend/src/test/java/com/google/rolecall/restcontrollers/IncrementUserLoginHttpRequestTests.java
@@ -1,8 +1,7 @@
 package com.google.rolecall.restcontrollers;
 
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doCallRealMethod;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
@@ -17,6 +16,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class IncrementUserLoginHttpRequestTests {
@@ -29,31 +33,20 @@ public class IncrementUserLoginHttpRequestTests {
 
   private String requestForm = "/api/increment-login?userid=%d";
 
-  private int[] userLogins;
-
-  private void mockSave(int id, User user) {
-    when(userRepo.findById(id)).thenReturn(Optional.of(user));
-  }
-
   @BeforeEach
   public void init() {
-    userLogins = new int[3];
-    for(int i=1; i <=userLogins.length; i++) {
-      userLogins[i-1] = i;
-
-      User user = mock(User.class);
-
+    for(int i=1; i <=3; i++) {
+      User user = new User();
+      for(int c =0; c<i; c++) {
+        user.incrementLogin();
+      }
       when(userRepo.findById(i)).thenReturn(Optional.of(user));
-      when(user.getId()).thenReturn(i);
-      mockSave(i, user);
-      when(user.getLoginCount()).thenAnswer(u -> {
-        return userLogins[user.getId()-1];
-      });
+      when(userRepo.save(user)).thenReturn(user);
     }
   }
     
   @Test
-  public void defaultGetUserLoginValue_success() throws Exception {
+  public void getUserLoginValue_success() throws Exception {
     // Setup
     String request1 = String.format(requestForm, 1);
     String request2 = String.format(requestForm, 3);
@@ -61,9 +54,41 @@ public class IncrementUserLoginHttpRequestTests {
     // Execute
     String response1 = this.restTemplate.getForObject(request1, String.class);
     String response2 = this.restTemplate.getForObject(request2, String.class);
-    System.out.println(response1);
+    
     // Assert
     assert(response1).equals("1");
     assert(response2).equals("3");
+  }
+
+  @Test
+  public void invalidUserLoginValue_failure() throws Exception {
+    // Setup
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    HttpEntity<Object> entity = new HttpEntity<Object>(headers);
+    String request = String.format(requestForm, 4);
+
+    // Execute
+    ResponseEntity<String> response = this.restTemplate.exchange(request,
+        HttpMethod.GET, entity, String.class);
+    
+    // Assert
+    assert(response.getStatusCode().isError());
+  }
+
+  @Test
+  public void incrementUserLoginValue_success() throws Exception {
+    // Setup
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    HttpEntity<Object> entity = new HttpEntity<Object>(headers);
+    String request = String.format(requestForm, 1);
+
+    // Execute
+    ResponseEntity<String> response = this.restTemplate.exchange(request,
+        HttpMethod.POST, entity, String.class);
+        
+    // Assert
+    assert(response.getBody()).equals("2");
   }
 }
