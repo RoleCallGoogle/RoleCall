7284b2de7f46a1ce49ffc476e3663a5b7559b382
Jared Hirsch <jaredhirsch@google.com>
2020-07-10 02:09:02 +0000

    Remove unneded imports

--- backend/src/test/java/com/google/rolecall/config/RepositoryConfigUnitTests.java
+++ backend/src/test/java/com/google/rolecall/config/RepositoryConfigUnitTests.java
@@ -1,32 +1,14 @@
 package com.google.rolecall.config;
 
-import com.google.api.gax.rpc.ApiException;
-import com.google.cloud.secretmanager.v1.AccessSecretVersionResponse;
-import com.google.cloud.secretmanager.v1.SecretPayload;
-import static com.google.common.truth.Truth.assertThat;
-import com.google.protobuf.ByteString;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 
+import static com.google.common.truth.Truth.assertThat;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.Assert.assertThrows;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-
-import java.io.IOException;
-
-import org.springframework.mock.env.MockEnvironment;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
